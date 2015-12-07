f3367f6d7b5eefac50c5fb92b2c8cba785cd02ac
Massaxe <gabbe.las.2011@gmail.com>
2015-12-07 21:08:04 +0100

    Small Update

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,26 +2,55 @@
 using System.Collections;
 
 public class EnemyAI : MonoBehaviour {
-    public Transform firstPos;
-    public Transform secondPos;
-    public bool moveFP;
-    public bool moveSP;
+    public Transform Pos;
+    public bool moveR;
+    public bool moveL;
     public Rigidbody2D enemy;
+    public float Dis = 10;
+    public float Speed = 100;
+    public bool Fired;
+    public GameObject Bullet;
+    public Transform Player;
+    public float bulletSpeed;
     // Use this for initialization
     void Start () {
         enemy = GetComponent<Rigidbody2D>();
+        Fired = false;
 	
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        Vector2 firstPosV = new Vector2(firstPos.position.x, transform.position.y);
-        if (moveFP)
-        {
-            Vector2 mf = Vector2.MoveTowards(transform.position, firstPosV, 10);
-            enemy.velocity += mf * 50 * Time.deltaTime;
-        }
-	
+	void Update () {
+        
     
 	}
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            print("Trigger stay works: " + col);
+            if (!Fired)
+            {
+                StartCoroutine(Fire());
+            }
+        }
+    }
+    IEnumerator Fire()
+    {
+
+        Fired = true;
+        FireFun();
+        yield return new WaitForSeconds(1);
+        Fired = false;
+
+
+
+
+    }
+    void FireFun()
+    {
+        Rigidbody2D clone = Instantiate(Bullet, transform.position, Quaternion.identity) as Rigidbody2D;
+        var playerDirection = Player.position - transform.position;
+        clone.velocity = playerDirection.normalized * bulletSpeed * Time.deltaTime;
+    }
 }
